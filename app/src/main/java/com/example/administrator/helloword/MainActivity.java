13f48db44cc8c0f0898543480bdf0b93cf8ea1e3
zhangeshihuai <464598892@qq.com>
2017-07-23 16:44:14 +0800

    2017/7/23提交

--- app/src/main/java/com/example/administrator/helloword/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloword/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.administrator.helloword;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.greenrobot.event.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EventBus.getDefault().isRegistered(this);//eventbus注册
         setContentView(R.layout.activity_main);
         //测试asdqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
         //服务器修改代码，客户端进行更新
+
     }
 }
+
---
+++ app/src/main/java/com/example/administrator/helloword/bean/MyMessage.java
@@ -0,0 +1,17 @@
+package com.example.administrator.helloword.bean;
+
+/**
+ * Created by Administrator on 2017/7/23 0023.
+ */
+
+public class MyMessage {
+    private String  message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
